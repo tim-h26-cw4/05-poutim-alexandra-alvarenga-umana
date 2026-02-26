962d075163eb9733d06055732c976a1463aaf296
AlexaAU <143153635+alexaAU@users.noreply.github.com>
2026-02-26 14:59:38 -0500

    étape 3- Poutine.js création init

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,6 +1,19 @@
 export default class Poutine {
-  constructor() {
+  constructor(element) {
     this.element = element;
     this.types = this.element.querySelectorAll('.js-btnPoutine');
+    selectedType = '';
+    console.log(this.types);
+    this.init();
   }
+
+  init() {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+      console.log(type);
+    }
+  }
+
+  selectType() {}
 }
