f930b301d6b439da8f1a320633028072ef714e88
AlexaAU <143153635+alexaAU@users.noreply.github.com>
2026-02-26 13:38:46 -0500

    Étape 1: Main.js création du constructor et du init

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,20 @@
-import Icons from "./utils/Icons.js";
+import Icons from './utils/Icons.js';
 
 // La ligne suivante devrait être au TOUT début du init() du Main
 Icons.load();
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const chefs = document.querySelectorAll('[data-component-chefs]');
+    for (let i = 0; i < chefs.length; i++) {
+      const chef = chef[i];
+      new Chefs();
+    }
+  }
+}
+
+new Main();
