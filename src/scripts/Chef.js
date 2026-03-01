fe4c76081e2a4df57a6e66fa9031c2557ac9bf7b
AlexaAU <alexa.alvarenga.umana@gmail.com>
2026-03-01 01:19:16 -0500

    Étape sendOrder - compte le nbr de poutine
    Non-fonctionnel

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -4,7 +4,7 @@ export default class Chef {
   constructor(element) {
     this.element = element;
     this.menu = [];
-    this.container = '';
+    this.container = this.element.querySelectorAll('.js-container');
 
     this.init();
   }
@@ -15,12 +15,33 @@ export default class Chef {
       const poutine = poutines[i];
       const instance = new Poutine(poutine);
       this.menu.push(instance);
-      //   console.log(this.menu);
+      // console.log(this.menu);
     }
-    // const btnCommande = this.element.querySelectorAll('.js-btnCommande');
-    // btnCommande.addEventListener('click', this.sendOrder.bind(this));
-    // console.log(btnCommande);
+
+    const btnCommandes = this.element.querySelectorAll('.js-btnCommande');
+    for (let i = 0; i < btnCommandes.length; i++) {
+      const btnCommande = btnCommandes[i];
+      btnCommande.addEventListener('click', this.sendOrder.bind(this));
+    }
   }
 
-  sendOrder() {}
+  sendOrder() {
+    // const nbrPoutine = Math.floor(this.menu);
+    // console.log(nbrPoutine);
+    // this.menu += 1;
+    for (let i = 0; i < this.menu.length; i++) {
+      const nbrPoutine = this.menu[i];
+      this.menu[i] += 1;
+      console.log(nbrPoutine);
+
+      for (let i = 0; i < this.container.length; i++) {
+        const container = this.container[i];
+        const p = document.createElement('p');
+        p.innerText = `Nombre total de poutine(s) : ${nbrPoutine}`;
+        container.innerHTML = '';
+        container.appendChild(p);
+        console.log(this.container);
+      }
+    }
+  }
 }
