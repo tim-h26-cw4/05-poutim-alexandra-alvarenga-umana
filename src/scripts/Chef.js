1801939068ef0e5af93966bb5fdc4f232c365948
AlexaAU <alexa.alvarenga.umana@gmail.com>
2026-03-04 17:28:17 -0500

    sendOrder  -  modification du code
    non-fonctionnel

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -5,7 +5,8 @@ export default class Chef {
     this.element = element;
     this.menu = [];
     this.container = this.element.querySelectorAll('.js-container');
-
+    this.isActive = false;
+    // this.sendOrder();
     this.init();
   }
 
@@ -25,27 +26,42 @@ export default class Chef {
   }
 
   sendOrder() {
-    //   if (typePoutine[i] == this.selectedType) {
-    //   nbrPoutine++;
-    // }
-    let nbrPoutine = 0;
+    let poutineSelect = true;
+    console.log(poutineSelect);
+
     for (let i = 0; i < this.menu.length; i++) {
-      const typePoutine = this.menu[i];
-
-      if (typePoutine[i] == this.types) {
-        nbrPoutine++;
-      }
-
-      console.log(nbrPoutine);
-
-      for (let i = 0; i < this.container.length; i++) {
-        const container = this.container[i];
-        const p = document.createElement('p');
-        p.innerText = `Nombre total de poutine(s) : ${nbrPoutine}`;
-        container.innerHTML = '';
-        container.appendChild(p);
-        console.log(this.container);
+      const poutine = this.menu[i];
+      if (poutine.isActive == true) {
+        poutineSelect = true;
+        poutine++;
       }
     }
+
+    // let nbrPoutine = 0;
+    // let poutineSelected = false;
+    // for (let i = 0; i < this.menu.length; i++) {
+    //   const typePoutine = this.menu[i];
+    //   if (typePoutine.isActive == true) {
+    //     poutineSelected = true;
+    //     nbrPoutine += 1;
+    //     console.log(poutineSelectede);
+    //   }
+
+    // let nbrPoutine = 0;
+    // for (let i = 0; i < this.menu.length; i++) {
+    //   const typePoutine = this.menu[i];
+    //   if (typePoutine[i] == this.types) {
+    //     nbrPoutine++;
+    //   }
+    //   console.log(nbrPoutine);
+    //   for (let i = 0; i < this.container.length; i++) {
+    //     const container = this.container[i];
+    //     const p = document.createElement('p');
+    //     p.innerText = `Nombre total de poutine(s) : ${nbrPoutine}`;
+    //     container.innerHTML = '';
+    //     container.appendChild(p);
+    //     console.log(this.container);
+    //   }
+    // }
   }
 }
